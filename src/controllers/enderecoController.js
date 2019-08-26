29020a94a7a0d771540bac65059e614e5818d3f8
Gustavo Santana <gustavofelipe68@gmail.com>
2019-08-26 17:52:00 -0300

    add auth middleware

---
+++ src/controllers/enderecoController.js
@@ -0,0 +1,7 @@
+const Endereco = require('./models/Endereco');
+
+modlue.exports = {
+    store(req, res){
+        const {  }
+    }
+}
---
+++ src/middlewares/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config({path: './src/config/.env'});
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader)
+        return res.status(401).send({error: "Token required."});
+
+    const parts = authHeader.split(' ');
+
+    if(!parts.length === 2)
+        return res.status(401).send({error: "Token error"});
+    
+    const [ scheme, token ] = parts;
+
+    console.log(scheme);
+    if(!/^Bearer$/i.test(scheme))
+        return res.status(401).send({error: "token malformed"});
+
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) return res.status(401).send({error: "Invalid Token"});
+        
+        req.userId = decoded.id;
+        return next();
+
+    });
+}
