086a6bd8e05d515bc75e7e7b95be52f657331730
Gustavo Santana <gustavofelipe68@gmail.com>
2019-09-04 15:11:05 -0300

    heroku error

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const routes = require('./routes');
 const app = express();
-mongoose.connect('mongodb+srv://nuiadm:KL1CAemvcW14l3gR@nui-lcjgk.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://nuiadm:KL1CAemvcW14l3gR@nui-lcjgk.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
 
 app.use(cors());
 app.use(express.json());
