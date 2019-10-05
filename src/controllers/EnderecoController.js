194874b15892b46bd4a3dec88f390bbd89e71685
Gustavo Santana <gustavofelipe68@gmail.com>
2019-10-05 19:08:27 -0300

    missing file

---
+++ src/controllers/EnderecoController.js
@@ -0,0 +1,21 @@
+const Endereco = require('./models/Endereco');
+
+modlue.exports = {
+    async store(req, res){
+      const { endereco, cidade, numero, cep, estado } = req.body
+      const {usuario_id} = req
+      
+      const endereco = new Endereco({
+        endereco,
+        cidade,
+        estado,
+        cep,
+        numero,
+      })
+      await endereco.save()
+      .then(() => {
+        return res.json({message: `${usuario_id}`})
+      })
+
+  }
+}
