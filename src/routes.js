a7672935b7b47a7c031b63c968b796a92c4cb2d2
Gustavo Santana <gustavofelipe68@gmail.com>
2019-10-05 19:06:50 -0300

    melhorias cadastro endereco

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ const routes = express.Router();
 
 routes.post('/usuario', UsuarioController.store);
 routes.post('/estabelecimento', EstabelecimentoController.store);
-routes.post('/endereco', EnderecoController.store);
+routes.post('/endereco', authMiddleware, EnderecoController.store);
 routes.get('/estabelecimento/:id', authMiddleware,EstabelecimentoController.show);
 routes.get('/estabelecimentos', authMiddleware,EstabelecimentoController.index);
 routes.post('/login', authController.login);
