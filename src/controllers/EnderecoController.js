44d5a807c4b261509a04d4c60e7fc0cbb8a45655
Gustavo Santana <gustavofelipe68@gmail.com>
2019-10-05 23:01:15 -0300

    debuging

--- src/controllers/EnderecoController.js
+++ src/controllers/EnderecoController.js
@@ -21,6 +21,28 @@ module.exports = {
         else
           return res.status(400).json({message: "Erro ao cadastrar endereço."})
       })
+  },
+  async update(req, res) {
+    const { endereco, cidade, numero, cep, estado } = req.body
+    const {usuario_id} = req
+    estabelecimento = await Estabelecimento.findOne({usuarioID: usuario_id})
+    const enderecoAtual = Endereco.findById(estabelecimento.enderecoId)
 
+    if(enderecoAtual){
+      enderecoAtual.endereco = endereco
+      enderecoAtual.cidade = cidade
+      enderecoAtual.numero = numero 
+      enderecoAtual.cep = cep
+      enderecoAtual.estado = estado
+      await enderecoAtual.save()
+      .then (() =>{
+        console.log('Worked')
+      })
+      .catch (() => {
+        console.log('do not Worked')
+
+      })
+
+    }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ const routes = express.Router();
 routes.post('/usuario', UsuarioController.store);
 routes.post('/estabelecimento', EstabelecimentoController.store);
 routes.post('/endereco', authMiddleware, EnderecoController.store);
+routes.patch('/endereco', authMiddleware, EnderecoController.update);
 routes.get('/estabelecimento/:id', authMiddleware,EstabelecimentoController.show);
 routes.get('/estabelecimentos', authMiddleware,EstabelecimentoController.index);
 routes.post('/login', authController.login);
